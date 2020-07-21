b96b6ff30243f5dbd250c9be4106652b3a2dc754
shad <shadrais321@outlook.com>
2020-07-22 01:47:52 +0530

    Updates

---
+++ precache-manifest.0c911b2f575013b500f80a7309ce77c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f49f4edcf1a0c654e7add6c416728d4",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "30496a6d8cca82be7172",
+    "url": "/robofriend/static/css/main.e7f78664.chunk.css"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/robofriend/static/js/2.526585eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.526585eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30496a6d8cca82be7172",
+    "url": "/robofriend/static/js/main.a4bd1244.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  }
+]);
