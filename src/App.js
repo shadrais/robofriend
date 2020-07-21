e4b7a33ac3c2d6993abd274139c4798d01ed07ab
Shad Rais <shadrais321@outlook.com>
2020-07-21 16:16:58 +0530

    Search Box Completed

--- src/App.js
+++ src/App.js
@@ -13,20 +13,27 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event.target.value);
-}
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+
 
     render() {
+
+        const filteredRobots = this.state.robots.filter(robot => {
+                return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchchange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
 
         );
     }
-
 }
+
+
 export default App;
